39c641295ce6e4efb4c1446a453458a1b8e72ef7
sstanton <steven@thesimpledev.com>
2025-09-23 17:07:02 -0400

    initial upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thesimpledev/secret-generator
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <count> <length>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	count, err := strconv.Atoi(os.Args[1])
+	if err != nil || count <= 0 {
+		fmt.Fprintln(os.Stderr, "Invalid count")
+		os.Exit(1)
+	}
+
+	length, err := strconv.Atoi(os.Args[2])
+	if err != nil || length <= 0 {
+		fmt.Fprintln(os.Stderr, "Invalid length")
+		os.Exit(1)
+	}
+
+	file, err := os.Create("secrets.md")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to create file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	for i := 0; i < count; i++ {
+
+		buf := make([]byte, length)
+		rand.Read(buf)
+		key := base64.StdEncoding.EncodeToString(buf)
+		key = key[:length]
+		_, err = fmt.Fprintln(file, key)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to write secret:", err)
+			os.Exit(1)
+		}
+
+	}
+
+}
